ddfff1c9ed54d060b536b20882b28263a8b836e0
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-03-30 09:28:01 +0200

    oijvopijofjqeojqwjfòqwjwdjiojfiòj3ifjr13j09¡3r+9º

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
 radio.setGroup(185)
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) > 75) {
+        radio.sendNumber(2)
+    }
+    if (input.rotation(Rotation.Roll) < -75) {
+        radio.sendNumber(3)
+    }
+})
