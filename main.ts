ca504f6c15cf35e334a527ddbb5c5137b10dc5c5
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-04-04 11:30:21 +0200

    jdjd

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(4)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(0)
 })
+music.playMelody("A F E F D G E F ", 120)
+basic.showString("Hola")
 radio.setGroup(185)
 basic.forever(function () {
     if (input.rotation(Rotation.Roll) > 75) {
